6475473b0595007e00cae246114149510e658330
Md Munna Mia <mdmunnamia987@gmail.com>
2024-11-13 21:43:52 +0600

    update on some cmd & work on main file

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2{
+
+	// If no command provided from user just return.
+	if len(os.Args) < 2 {
 		fmt.Println("Usages: todo <command> [arguments]")
+		return
+	}
+
+	command := os.Args[1] // taking the command (add, list, delete...)
+
+	switch command {
+	case "add":
 	}
 }
